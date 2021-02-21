12412a2b8aea638ff4fbf4dd54f4d7bbe77cb485
启哲 陈 <2261150574@qq.com>
2021-02-21 21:38:01 +0800

    after mobileNet training

--- slim/nets/inception_v1.py
+++ slim/nets/inception_v1.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import tf_slim as slim
 
 from nets import inception_utils
--- slim/nets/mobilenet/mobilenet_v2.py
+++ slim/nets/mobilenet/mobilenet_v2.py
@@ -27,7 +27,7 @@ from __future__ import print_function
 import copy
 import functools
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import tf_slim as slim
 
 from nets.mobilenet import conv_blocks as ops
